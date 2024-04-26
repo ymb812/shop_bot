9a0b029f7c310503a1d18fae03f17a7704a729f0
Kotov Ilya <the3k27@gmail.com>
2024-04-26 20:30:51 +0300

    Fix button

--- app_bot/core/dialogs/support_dialog.py
+++ app_bot/core/dialogs/support_dialog.py
@@ -1,6 +1,6 @@
 from aiogram_dialog import Dialog, Window
 from aiogram_dialog.widgets.text import Const
-from aiogram_dialog.widgets.kbd import Button, SwitchTo
+from aiogram_dialog.widgets.kbd import Button, SwitchTo, Start
 from aiogram_dialog.widgets.input import TextInput
 from core.dialogs.callbacks import SupportCallbackHandler
 from core.states.support import SupportStateGroup
@@ -14,7 +14,7 @@ support_dialog = Dialog(
         Const(text=_('PICK_ACTION')),
         Button(Const(text=_('WRITE_BUTTON')), id='go_to_manager', on_click=SupportCallbackHandler.menu_buttons_handler),
         SwitchTo(Const(text=_('FEED_BACK_BUTTON')), id='go_to_feed_back', state=SupportStateGroup.input_phone),
-        SwitchTo(Const(text=_('BACK_BUTTON')), id='switch_to_menu', state=MainMenuStateGroup.menu),
+        Start(Const(text=_('BACK_BUTTON')), id='switch_to_menu', state=MainMenuStateGroup.menu),
         state=SupportStateGroup.menu,
     ),
 
